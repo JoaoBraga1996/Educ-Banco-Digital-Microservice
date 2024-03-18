0a92a25a859a62120878971c0ea8fd2af2913653
unknown <joaofelipebraga1996@gmail.com>
2024-03-17 23:00:23 -0300

    Banco Digital 360

--- msconta/msconta/src/main/java/com/joaofelipebraga/msconta/mqueue/EmissaoCartaoSubscriber.java
+++ msconta/msconta/src/main/java/com/joaofelipebraga/msconta/mqueue/EmissaoCartaoSubscriber.java
@@ -1,39 +1,34 @@
 package com.joaofelipebraga.msconta.mqueue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joaofelipebraga.msconta.dtos.DadosEmissaoCartao;
 import com.joaofelipebraga.msconta.entities.Cartao;
 import com.joaofelipebraga.msconta.repositories.CartaoRepository;
-import com.joaofelipebraga.msconta.services.exceptions.ResourceNotFoundException;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Component
 public class EmissaoCartaoSubscriber {
 
-	@Autowired
-	CartaoRepository repository;
+    private static final Logger log = LoggerFactory.getLogger(EmissaoCartaoSubscriber.class);
 
-	@RabbitListener(queues = "${mq.queues.emissao-cartoes}")
-	public void receberSolicitacaoEmissao(@Payload String payload) {
+    @Autowired
+    CartaoRepository repository;
 
-		try {
-			var mapper = new ObjectMapper();
-			DadosEmissaoCartao dados = mapper.readValue(payload, DadosEmissaoCartao.class);
-			Cartao cartao = repository.findById(dados.getId())
-					.orElseThrow(() -> new EntityNotFoundException("Entidade não achada"));
-
-			repository.save(cartao);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receberSolicitacaoEmissao(@Payload String payload) {
+        try {
+            var mapper = new ObjectMapper();
+            DadosEmissaoCartao dados = mapper.readValue(payload, DadosEmissaoCartao.class);
+            Cartao cartao = repository.findById(dados.getId()).orElseThrow();
+            repository.save(cartao);
+        } catch (Exception e) {
+            log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
+        }
+    }
 }
